79712b3ff37ed8337ecca4bd632a25a0a0cdc5e0
Itagaki Daiki <d.itagaki.sys19@morijyobi.ac.jp>
2020-06-29 12:21:13 +0900

    dd

--- delete.php
+++ delete.php
@@ -3,21 +3,20 @@ $dsn = 'mysql:dbname=sample_db;host=localhost;';
 $user = 'itagakidaiki';
 $password = 'morijyobi';
 try {
-        $dbh = new PDO($dsn, $user, $password);
-        $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $id = $_GET['id'];
+    $id = $_GET['id'];
 
-        $sql = "delete from user where id=:id";
-        $stmt=$dbh->prepare($sql,);
-        $prams=array(':id'=>$id);
-        $stmt->execute($prams);
+    $sql = "delete from user where id=:id";
+    $stmt = $dbh->prepare($sql);
+    $prams = array(':id'=> $id);
+    $stmt->execute($prams);
 
-        header('Location: index.php?fg=1');
+    header('Location: index.php?fg=1');
     
 } catch (PDOException $e) {
-        print "Failed: " . $e->getMessage() . "\n";
-        exit();
-    }
-
+	header('Location: index.php?fg=2?err='.$e->getMessage());
+	exit();
+}
 ?>
