eb51337c16ff0659ba6f5d318b5192c9855b6d64
Itagaki Daiki <d.itagaki.sys19@morijyobi.ac.jp>
2020-06-29 11:22:57 +0900

    the commit

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ $user = 'itagakidaiki';
 $password = 'morijyobi';
 try {
     $dbh = new PDO($dsn, $user, $password);
-        print "Success\n";
         $sql = "select * from user;";
         $result = $dbh->query($sql);
     } catch (PDOException $e) {
@@ -98,7 +97,29 @@ try {
                 <p>delete</p>
             </div>
             <div class="tab-pane" id="update">
-                <p>update</p>
+            <form class="mt-3" action="./update.php" method="POST">
+					<div class="form-group row">
+						<label for="id" class="col-sm-2 col-form-label">ID</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="id" name="id">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="name" class="col-sm-2 col-form-label">Your Name</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="name" name="name">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="age" class="col-sm-2 col-form-label">Age</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="age" name="age">
+						</div>
+					</div>
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary btn-block">Update</button>
+					</div>
+				</form>
             </div>
         </div>
       
